a50b2bb42f9bae083439823326133f8391fffb0a
jossyahia <jossyahia@gmail.com>
2023-04-21 03:28:01 +0100

    div

--- app/page.jsx
+++ app/page.jsx
@@ -30,7 +30,11 @@ export default function Home() {
           </a>
         </div>
       </div>
-
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <h1>
+          This is fastfast the only solution you will need for your business growth.
+        </h1>
+      </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -58,7 +62,7 @@ export default function Home() {
           <p
             className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
           >
-            Book a Dispach Rider for instant Delivery.
+            Request for a Dispach Rider for instant Delivery of your goods.
           </p>
         </a>
 
@@ -107,7 +111,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+            About Us{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -115,7 +119,8 @@ export default function Home() {
           <p
             className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
           >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Want to know more about us and how we can help you grow you
+            business?.
           </p>
         </a>
       </div>
